8299f2831bb4a351dd88e40b6b83e84cb0bd27aa
Paul Raju <paulrj1994@gmail.com>
2023-09-26 11:14:28 +0530

    Website work in progress

--- arm-tuition/src/app/app.component.ts
+++ arm-tuition/src/app/app.component.ts
@@ -9,4 +9,43 @@ export class AppComponent {
   title = 'arm-tuition';
 
   public mobileMenuOpened = false;
+  public toggleSidebar() {
+    this.mobileMenuOpened = !this.mobileMenuOpened;
+    document.getElementById('sidebar_overlay_id')?.classList.toggle('anim')
+  }
+
+  public slideIndex = 1;
+  ngOnInit() {
+    this.showDivs(this.slideIndex);
+    // setInterval(() => {
+    //   this.showDivs(++this.slideIndex);
+    // }, 5000)
+  }
+
+  plusDivs(count: number) {
+    this.showDivs(this.slideIndex += count);
+  }
+
+  currentDiv(count: number) {
+    this.showDivs(this.slideIndex = count);
+  }
+
+  showDivs(count: number) {
+    var i;
+    var x = document.getElementsByClassName("carousel-image");
+    var dots = document.getElementsByClassName("slider-thumb");
+    if (count > x.length) { this.slideIndex = 1 }
+    if (count < 1) { this.slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+      (x[i] as HTMLElement).style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    (x[this.slideIndex - 1] as HTMLElement).style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
+  }
+  public goToLink(url: string){
+    window.open(url, "_blank");
+}
 }
