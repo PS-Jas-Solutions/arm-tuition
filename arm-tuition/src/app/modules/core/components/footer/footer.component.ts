9482ccc43a3a8063542f359e92fd51d7effc1cb8
Paul Raju <paulrj1994@gmail.com>
2023-09-28 15:11:29 +0530

    social links added at footer

--- arm-tuition/src/app/modules/core/components/footer/footer.component.ts
+++ arm-tuition/src/app/modules/core/components/footer/footer.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  public gotoFacebookPage() {
+    this.goToLink('https://www.facebook.com/armclasses/photos');
+  }
+  public gotoInstagramPage() {
+    this.goToLink('https://www.instagram.com/antonyrajumathclasses/');
+  }
+  public gotoGmail() {
+    this.goToLink('https://mail.google.com/mail/u/0/#inbox?compose=new');
+  }
+  public gotoWhatsapp() {
+    this.goToLink('https://www.whatsapp.com/');
+  }
+  public gotoYoutubeChannel() {
+    this.goToLink('https://www.youtube.com/@ANTONYRAJUMATHCLASSESARM');
+  }
+  public goToLink(url: string) {
+    window.open(url, "_blank");
+  }
 }
